24cf6ea98ce44a08319f9552e1beca83af269b01
Ivan <pavlovivaneu@gmail.com>
2022-08-30 23:32:35 +0200

    popup_menu.1.1

--- src/components/Sort.jsx
+++ src/components/Sort.jsx
@@ -1,26 +1,19 @@
 import React, { useState } from 'react';
 
 function Sort() {
-  const listMenu = ['популярне', 'вартість', 'алфавіт'];
-  const [activeMenu, setActiveMeny] = useState(0);
+  const listMenu = ['популярністю', 'вартістю', 'алфавітом'];
+  const [activeMenu, setActiveMenu] = useState(0);
   const [open, setOpen] = useState(false);
+  const onClickMenu = (i) => {
+    setActiveMenu(i);
+    setOpen(false);
+  };
 
   return (
     <div className="sort">
       <div className="sort__label">
-        <svg
-          width="10"
-          height="6"
-          viewBox="0 0 10 6"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg">
-          <path
-            d="M10 5C10 5.16927 9.93815 5.31576 9.81445 5.43945C9.69075 5.56315 9.54427 5.625 9.375 5.625H0.625C0.455729 5.625 0.309245 5.56315 0.185547 5.43945C0.061849 5.31576 0 5.16927 0 5C0 4.83073 0.061849 4.68424 0.185547 4.56055L4.56055 0.185547C4.68424 0.061849 4.83073 0 5 0C5.16927 0 5.31576 0.061849 5.43945 0.185547L9.81445 4.56055C9.93815 4.68424 10 4.83073 10 5Z"
-            fill="#2C2C2C"
-          />
-        </svg>
-        <b>Сортування по:</b>
-        <span onClick={() => setOpen(!open)}>популярности</span>
+        <b>Сортування за:</b>
+        <span onClick={() => setOpen(!open)}>{listMenu[activeMenu]}</span>
       </div>
       {open && (
         <div className="sort__popup">
@@ -28,7 +21,7 @@ function Sort() {
             {listMenu.map((name, i) => (
               <li
                 key={i}
-                onClick={() => setActiveMeny(i)}
+                onClick={() => onClickMenu(i)}
                 className={activeMenu === i ? 'active' : ''}>
                 {name}
               </li>
